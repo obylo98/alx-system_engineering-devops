29ceb2adfe96fda129958236f307be371005e3dd
Obinna Andrew Onyekwere <obinnaandrewonyekwere@gmail.com>
2023-11-29 01:54:57 +0100

    second Update 1-install_a_package.pp
    update to fix the error on flask --version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,20 @@
 #!usr/bin/pup
-#Install flask / Version must be 2.1.0
-package { 'flask':
+# 1-install_a_package.pp
+
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask and a compatible version of Werkzeug using pip
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1', # or another version that is compatible with Flask 2.1.0
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
